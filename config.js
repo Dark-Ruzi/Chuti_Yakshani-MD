cd778d2feccc5bb18aad7cbe856246c71feebc16
Dark-Ruzi <pasindusbandara923@gmail.com>
2025-03-01 11:13:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 // Export the SESSION_ID environment variable
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'mBYGhIoJ#N973VZc8CG7894Wozlqyv4GdmqAKVsqb4ZnaFsFmmB4',
+    SESSION_ID: process.env.SESSION_ID || '7zIjyCKB#vdrBB6qfbxnLu6cTv3cADArBGq810AJ03abU3O_UEXU',
     MONGODB: process.env.MONGODB || "mongodb://mongo:lXeSCuCxsffPJwNIsfrAGyZxpHtmzdIJ@autorack.proxy.rlwy.net:10236",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "cc1a20a7",
     READ_CMD: process.env.READ_CMD || "true",
